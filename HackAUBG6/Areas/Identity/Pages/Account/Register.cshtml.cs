152b0d625a8cd900f059fa884345ea30ef2ccf99
RiverAnya <yana.yancheva.yaneva@gmail.com>
2024-03-31 00:01:50 +0200

    proba

--- HackAUBG6/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ HackAUBG6/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -112,7 +112,6 @@ namespace HackAUBG6.Areas.Identity.Pages.Account
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
