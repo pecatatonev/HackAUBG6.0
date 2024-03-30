88c2d17cb2878af8e48d1f6b5e6a528f6740cdc6
Petko Tonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-30 16:22:33 +0200

    Adding extensions and utils

---
+++ HackAUBG6.Util/JsonSerializationExtension.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Xml;
+using JsonConverter = Newtonsoft.Json.JsonConverter;
+
+namespace HackAUBG6.Util
+{
+    public static class JsonSerializationExtension
+    {
+        public static string SerializeToJson<T>(this T obj)
+        {
+            var jsonSerializer = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                Converters = new List<JsonConverter>()
+                {
+                    new StringEnumConverter()
+                }
+            };
+
+            string result = JsonConvert.SerializeObject(obj, jsonSerializer);
+
+            return result;
+        }
+
+        public static T DeserializeFromJson<T>(this string jsonString)
+        {
+            var jsonSerializer = new JsonSerializerSettings()
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = Newtonsoft.Json.Formatting.Indented
+            };
+
+            T result = JsonConvert.DeserializeObject<T>(jsonString, jsonSerializer);
+
+            return result;
+        }
+    }
+}
---
+++ HackAUBG6/Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,45 @@
+﻿using HackAUBG6.Infrastructure.Data;
+using HackAUBG6.Infrastructure.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using NuGet.Protocol.Core.Types;
+
+namespace HackAUBG6.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection service)
+        {
+            service.AddLogging();
+
+
+            return service;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            return services;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services
+                 .AddDefaultIdentity<ApplicationUser>(options =>
+                 {
+                     options.SignIn.RequireConfirmedAccount = false;
+                     options.SignIn.RequireConfirmedEmail = false;
+                     options.Password.RequireDigit = false;
+                     options.Password.RequireNonAlphanumeric = false;
+                     options.Password.RequiredLength = 6;
+                 })
+                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
