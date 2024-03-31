e782550535a3bd2e756d8bbb1e475050e42031f3
Petko Tonev <114527717+pecatatonev@users.noreply.github.com>
2024-03-31 09:56:38 +0300

    Adding more logic

--- HackAUBG6/Controllers/SortingController.cs
+++ HackAUBG6/Controllers/SortingController.cs
@@ -10,7 +10,7 @@ using System.Text;
 
 namespace HackAUBG6.Controllers
 {
-    [Microsoft.AspNetCore.Authorization.Authorize]
+    [Authorize]
     public class SortingController : Controller
     {
         private readonly ILogger<HomeController> logger;
